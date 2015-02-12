df74ee266eea30eb45e637c55b7c89e54834df01
Gary Chang <gary.chang.355@gmail.com>
2015-02-11 23:20:27 -0500

    Finished flee

--- Assets/Scripts/Flee.cs
+++ Assets/Scripts/Flee.cs
@@ -6,7 +6,10 @@ public class Flee : MonoBehaviour
 {
     public Transform target;
     Npc npc;
-    
+
+    Vector3 velocity;
+    float angularVelocity;
+
     void Start ()
     {
         npc = GetComponent<Npc>();
@@ -46,7 +49,55 @@ public class Flee : MonoBehaviour
 
     void SteeringFlee()
     {
+        Vector3 fleeDirection = transform.position - target.position;
+        Align (fleeDirection);
+
+        Vector3 acceleration = npc.MaxAcceleration * fleeDirection.normalized;
+        velocity += acceleration * Time.deltaTime;
+        if(velocity.magnitude > npc.MaxAcceleration)
+        {
+            velocity = velocity.normalized * npc.MaxAcceleration;
+        }
+
+        if(fleeDirection.magnitude < npc.FleeDistanceThreshold)
+        {
+            // Step away directly even if there's sidestepping (behavior C.i)
+            transform.Translate (velocity * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            // Face away from target and move away (behavior C.ii)
+            transform.Translate (transform.forward * velocity.magnitude * Time.deltaTime, Space.World);
+        }
 
     }
 
+    void Align(Vector3 targetAlignment)
+    {
+        int sign = Vector3.Cross(transform.forward, targetAlignment).y > 0 ? 1 : -1;
+        float angleDifference = Vector3.Angle (transform.forward, targetAlignment);
+        
+        if(angleDifference > npc.AngleThreshold)
+        {
+            float goalAngularVelocity = npc.MaxAngularVelocity * angleDifference / npc.SlowDownOrientation;
+            float angularAcceleration = (goalAngularVelocity - angularVelocity) / npc.AngularTimeToTarget;
+            
+            if(angularAcceleration > npc.MaxAngularAcceleration)
+            {
+                angularAcceleration = npc.MaxAngularAcceleration;
+            }
+            
+            angularVelocity += angularAcceleration * Time.deltaTime;
+            if(angularVelocity > npc.MaxAngularVelocity)
+            {
+                angularVelocity = npc.MaxAngularVelocity;
+            }
+            
+            transform.Rotate (transform.up, angularVelocity * Time.deltaTime * sign, Space.World);
+        }
+        else
+        {
+            transform.rotation = Quaternion.LookRotation(targetAlignment);
+        }
+    }
 }
